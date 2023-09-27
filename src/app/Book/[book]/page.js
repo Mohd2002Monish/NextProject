d57fc64bc11792ceafe573eadc755baa5505cbd1
Mohd Monish <97455677+Mohd2002Monish@users.noreply.github.com>
2023-09-27 17:22:04 +0530

    Final Commit

--- src/app/Book/[book]/page.js
+++ src/app/Book/[book]/page.js
@@ -1,6 +1,6 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+'use client';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
   Typography,
   Card,
@@ -8,39 +8,41 @@ import {
   CardMedia,
   Button,
   TextField,
-} from "@mui/material";
+} from '@mui/material';
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
+import ModalDialog from '@mui/joy/ModalDialog';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
 
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import Fade from "@mui/material/Fade";
-
-import { useRouter } from "next/navigation";
-import { Grid } from "@mui/joy";
+import { useRouter } from 'next/navigation';
+import { Grid, List } from '@mui/joy';
 function Page({ params }) {
+  const [layout, setLayout] = React.useState(undefined);
+
   const [found, setFound] = useState(false);
   const [book, setBook] = useState({});
   const [open, setOpen] = useState(false);
   const router = useRouter();
   const [bookData, setBookData] = useState({
-    authorName: "",
-    authorBiography: "",
-    authorNationality: "",
-    authorBirthDate: "",
-    title: "",
-    coverImage: "",
-    genre: "Fantasy",
-    publicationDate: "",
+    authorName: '',
+    authorBiography: '',
+    authorNationality: '',
+    authorBirthDate: '',
+    title: '',
+    coverImage: '',
+    genre: 'Fantasy',
+    publicationDate: '',
     isFree: false,
   });
 
@@ -58,15 +60,19 @@ function Page({ params }) {
     axios
       .put(`https://wednesday-qcez.onrender.com/book/${params.book}`, bookData)
       .then((response) => {
-        console.log("PUT request successful", response.data);
+        console.log('PUT request successful', response.data);
         setOpen(false);
         FetchData();
       })
       .catch((error) => {
-        console.error("Error updating resource:", error);
+        console.error('Error updating resource:', error);
       });
   };
-  const handleOpen = () => setOpen(true);
+  const handleOpen = () => {
+    setOpen(true);
+
+    setLayout('center');
+  };
 
   const handleClose = () => {
     setOpen(false);
@@ -76,11 +82,11 @@ function Page({ params }) {
     axios
       .delete(`https://wednesday-qcez.onrender.com/book/${id}`)
       .then((response) => {
-        console.log("Delete request successful", response.data);
-        router.push("/");
+        console.log('Delete request successful', response.data);
+        router.push('/');
       })
       .catch((error) => {
-        console.error("Error deleting resource:", error);
+        console.error('Error deleting resource:', error);
       });
   };
   const FetchData = async () => {
@@ -94,7 +100,7 @@ function Page({ params }) {
       setBookData(data);
       setFound(true);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
     }
   };
   useEffect(() => {
@@ -104,20 +110,20 @@ function Page({ params }) {
     <div>
       {found ? (
         <div>
-          {" "}
+          {' '}
           <div
             style={{
-              display: "flex",
+              display: 'flex',
 
-              justifyContent: "center",
+              justifyContent: 'center',
             }}
           >
-            {" "}
+            {' '}
             <Card
               sx={{
-                display: "flex",
+                display: 'flex',
                 maxWidth: 600,
-                marginTop: "20px",
+                marginTop: '20px',
               }}
             >
               <CardMedia
@@ -149,16 +155,16 @@ function Page({ params }) {
                   Publication Date: {book.publicationDate}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  {book.isFree ? "Free" : "Not Free"}
+                  {book.isFree ? 'Free' : 'Not Free'}
                 </Typography>
               </CardContent>
             </Card>
           </div>
           <div>
             <div>
-              <Modal sx={{ height: "800px" }} open={open} onClose={handleClose}>
+              <Modal open={!!layout} onClose={handleClose}>
                 <Fade in={open}>
-                  <Box sx={style}>
+                  <ModalDialog sx={style} layout={layout}>
                     <Typography
                       id="transition-modal-title"
                       variant="h6"
@@ -166,133 +172,132 @@ function Page({ params }) {
                     >
                       Make Changes To Book
                     </Typography>
-                    <Typography
-                      id="transition-modal-description"
-                      sx={{ mt: 4 }}
+
+                    <List
+                      sx={{
+                        overflow: scroll ? 'scroll' : 'initial',
+                        mx: 'calc(-1 * var(--ModalDialog-padding))',
+                        px: 'var(--ModalDialog-padding)',
+                      }}
                     >
-                      <form onSubmit={handleSubmit}>
-                        <Grid container spacing={2}>
-                          <Grid item xs={12}>
-                            <TextField
-                              fullWidth
-                              label="Author Name"
-                              name="authorName"
-                              variant="outlined"
-                              value={bookData.authorName}
-                              onChange={handleInputChange}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <TextField
-                              fullWidth
-                              label="Author Biography"
-                              name="authorBiography"
-                              variant="outlined"
-                              multiline
-                              rows={4}
-                              value={bookData.authorBiography}
-                              onChange={handleInputChange}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <TextField
-                              fullWidth
-                              label="Author Nationality"
-                              name="authorNationality"
-                              variant="outlined"
-                              value={bookData.authorNationality}
-                              onChange={handleInputChange}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <TextField
-                              fullWidth
-                              label="Author Birth Date"
-                              name="authorBirthDate"
-                              variant="outlined"
-                              value={bookData.authorBirthDate}
-                              onChange={handleInputChange}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <TextField
-                              fullWidth
-                              label="Title"
-                              name="title"
-                              variant="outlined"
-                              value={bookData.title}
-                              onChange={handleInputChange}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <TextField
-                              fullWidth
-                              label="Cover Image URL"
-                              name="coverImage"
-                              variant="outlined"
-                              value={bookData.coverImage}
-                              onChange={handleInputChange}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <TextField
-                              fullWidth
-                              label="Genre"
-                              name="genre"
-                              variant="outlined"
-                              value={bookData.genre}
-                              onChange={handleInputChange}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <TextField
-                              fullWidth
-                              label="Publication Date"
-                              name="publicationDate"
-                              variant="outlined"
-                              value={bookData.publicationDate}
-                              onChange={handleInputChange}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <TextField
-                              fullWidth
-                              label="Is Free"
-                              name="isFree"
-                              variant="outlined"
-                              value={bookData.isFree}
-                              onChange={handleInputChange}
-                            />
-                          </Grid>
-                          <Grid item xs={12}>
-                            <Box textAlign="center">
-                              <Button
-                                variant="contained"
-                                color="primary"
-                                type="submit"
-                              >
-                                Save
-                              </Button>
-                            </Box>
-                          </Grid>
-                        </Grid>
+                      <form onSubmit={handleSubmit} container spacing={2}>
+                        <TextField
+                          fullWidth
+                          label="Author Name"
+                          name="authorName"
+                          variant="outlined"
+                          value={bookData.authorName}
+                          onChange={handleInputChange}
+                        />
+
+                        <TextField
+                          sx={{ marginTop: '20px' }}
+                          fullWidth
+                          label="Author Biography"
+                          name="authorBiography"
+                          variant="outlined"
+                          multiline
+                          rows={4}
+                          value={bookData.authorBiography}
+                          onChange={handleInputChange}
+                        />
+
+                        <TextField
+                          sx={{ marginTop: '20px' }}
+                          fullWidth
+                          label="Author Nationality"
+                          name="authorNationality"
+                          variant="outlined"
+                          value={bookData.authorNationality}
+                          onChange={handleInputChange}
+                        />
+
+                        <TextField
+                          sx={{ marginTop: '20px' }}
+                          fullWidth
+                          label="Author Birth Date"
+                          name="authorBirthDate"
+                          variant="outlined"
+                          value={bookData.authorBirthDate}
+                          onChange={handleInputChange}
+                        />
+
+                        <TextField
+                          sx={{ marginTop: '20px' }}
+                          fullWidth
+                          label="Title"
+                          name="title"
+                          variant="outlined"
+                          value={bookData.title}
+                          onChange={handleInputChange}
+                        />
+
+                        <TextField
+                          sx={{ marginTop: '20px' }}
+                          fullWidth
+                          label="Cover Image URL"
+                          name="coverImage"
+                          variant="outlined"
+                          value={bookData.coverImage}
+                          onChange={handleInputChange}
+                        />
+
+                        <TextField
+                          sx={{ marginTop: '20px' }}
+                          fullWidth
+                          label="Genre"
+                          name="genre"
+                          variant="outlined"
+                          value={bookData.genre}
+                          onChange={handleInputChange}
+                        />
+
+                        <TextField
+                          sx={{ marginTop: '20px' }}
+                          fullWidth
+                          label="Publication Date"
+                          name="publicationDate"
+                          variant="outlined"
+                          value={bookData.publicationDate}
+                          onChange={handleInputChange}
+                        />
+
+                        <TextField
+                          sx={{ marginTop: '20px' }}
+                          fullWidth
+                          label="Is Free"
+                          name="isFree"
+                          variant="outlined"
+                          value={bookData.isFree}
+                          onChange={handleInputChange}
+                        />
+
+                        <Box sx={{ marginTop: '20px' }} textAlign="center">
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            type="submit"
+                          >
+                            Save
+                          </Button>
+                        </Box>
                       </form>
-                    </Typography>
-                  </Box>
+                    </List>
+                  </ModalDialog>
                 </Fade>
               </Modal>
             </div>
           </div>
           <div
             style={{
-              display: "flex",
-              justifyContent: "space-evenly",
-              margin: "20px",
-              gap: "20px",
+              display: 'flex',
+              justifyContent: 'space-evenly',
+              margin: '20px',
+              gap: '20px',
             }}
           >
             <Button variant="outlined" color="success" onClick={handleOpen}>
-              Update Book{" "}
+              Update Book{' '}
             </Button>
             <Button
               onClick={() => removeBook(book._id)}
@@ -304,7 +309,7 @@ function Page({ params }) {
           </div>
         </div>
       ) : (
-        <h1 style={{ textAlign: "center" }}>Loading...</h1>
+        <h1 style={{ textAlign: 'center' }}>Loading...</h1>
       )}
     </div>
   );
